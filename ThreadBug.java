33a4d57dcb78001986955e1814f231ce7c9244eb
halima khanmammadova <hkhanmammadova13941@ada.edu.az>
2026-05-01 13:12:40 +0400

    Exercise 7

---
+++ ThreadBug.java
@@ -0,0 +1,11 @@
+public class ThreadBug {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            System.out.println("Running in another thread");
+        });
+
+        t.start();
+
+        System.out.println("Main finished");
+    }
+}
