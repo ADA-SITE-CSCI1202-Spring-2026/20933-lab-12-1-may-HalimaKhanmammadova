6a6a5bf5efa22b078a628548b5585426733fc06b
halima khanmammadova <hkhanmammadova13941@ada.edu.az>
2026-05-01 13:09:51 +0400

    Exercise 4

---
+++ LambdaFixTask.java
@@ -0,0 +1,11 @@
+@FunctionalInterface
+interface NumberChecker {
+    boolean check(int number);
+}
+
+public class LambdaFixTask {
+    public static void main(String[] args) {
+        NumberChecker isEven = number -> number % 2 == 0;
+        System.out.println(isEven.check(10));
+    }
+}
